89f7332b295dcd07ac12a07f63b59a4af537980f
Vitor Mac <5874907+listanegra@users.noreply.github.com>
2022-06-23 23:44:17 -0300

    Firebase app initialization

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import { initializeApp as FirebaseWeb } from "firebase/app";
+import { initializeApp as FirebaseAdmin, applicationDefault } from "firebase-admin/app";
+
+FirebaseAdmin({ credential: applicationDefault() });
+FirebaseWeb({ apiKey: process.env['FIREBASE_API_KEY'] });
 
 const app = express();
 app.use(express.json());
