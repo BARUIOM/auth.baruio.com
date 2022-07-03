edd9d34835477d591d0b6828491095f1a6e9e868
Vitor Mac <5874907+listanegra@users.noreply.github.com>
2022-07-02 21:37:05 -0300

    Disable CORS on non-production environment

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import cors from "cors";
 import express from "express";
 import { initializeApp as FirebaseWeb } from "firebase/app";
 import { initializeApp as FirebaseAdmin, applicationDefault } from "firebase-admin/app";
@@ -13,11 +14,18 @@ FirebaseWeb({ apiKey: process.env['FIREBASE_API_KEY'] });
 
 const app = express();
 app.use(express.json());
+app.disable('x-powered-by');
+
+const NODE_ENV = process.env['NODE_ENV'];
+
+if (NODE_ENV !== 'production')
+    app.use(cors());
 
 app.use(SignInService);
 app.use(TokenService);
 
-const server = app.listen(3000, () => {
+const PORT = process.env['PORT'] || 3001;
+const server = app.listen(PORT, () => {
     const address = server.address();
     console.log("Running BARUIO's authentication API", address);
 });
